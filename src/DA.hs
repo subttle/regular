b5c488182cf5d5988b2efc530f975e0ececace39
Ian Treyball <ian.treyball@columbia.edu>
2019-03-02 17:49:43 -0500

    progress with DA

--- src/DA.hs
+++ src/DA.hs
@@ -5,8 +5,10 @@
 module DA where
 
 -- import           Common
-import           Language
+import qualified Language
+import           Language (ℒ)
 -- import           Finite
+-- import           Data.Bool.Unicode
 -- import           Data.Functor.Contravariant
 -- import           Data.Functor.Contravariant.Divisible
 -- import           Data.Void
@@ -15,6 +17,8 @@ import           Language
 -- http://www.few.vu.nl/~cgr600/linkedfiles/swansea_slides.pdf
 -- Using Proofs by Coinduction to Find “Traditional” Proofs Clemens Grabmayer
 -- And some work by Jan Rutten
+-- Automata and Coinduction (An Exercise in Coalgebra) J.J.M.M. Rutten 
+-- http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.221.6957&rep=rep1&type=pdf
 
 -- Deterministic Automaton
 -- N.B. that DA does not require `q` or `s` to be finite, but when they are
@@ -29,3 +33,16 @@ language ∷ DA q s → q → ℒ s
 language (DA o _) q []      = o q
 language (DA o t) q (a : w) = language (DA o t) (t q a) w
 
+-- "automaton of languages"
+automaton ∷ DA (ℒ s) s
+automaton = DA { output     = Language.nullable
+               , transition = Language.derivative
+               }
+
+empty ∷ DA q s
+empty = DA { output     = const False
+           , transition = const
+           }
+
+complement ∷ DA q s → DA q s
+complement m@(DA o _) = m { output = not . o }
