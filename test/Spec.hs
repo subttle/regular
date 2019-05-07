aecd78ba1ba0c4e7f59ba2b0579aef650b6032d6
Ian Treyball <subttle@users.noreply.github.com>
2019-05-07 17:05:54 -0400

    improve some testing

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,7 +9,7 @@ import           DFA
 -- import qualified RegExp as RE
 -- import           Common
 import           Finite
-import           Examples
+-- import           Examples
 import           Data.Set
 import           Config
 import           EasyTest
@@ -17,8 +17,10 @@ import           EasyTest
 main ∷ IO ()
 main = run suite
 
-suite :: Test ()
-suite = tests [ scope "DFA.literal"   . expect $ Config.language (DFA.literal True)  == [[True]]
+suite ∷ Test ()
+suite = tests [ scope "DFA.empty"     . expect $ Config.language DFA.empty          == ([]   ∷ [[Bool]])
+              , scope "DFA.epsilon"   . expect $ Config.language DFA.epsilon        == ([[]] ∷ [[Bool]])
+              , scope "DFA.literal"   . expect $ Config.language (DFA.literal True) == [[True]]
               -- https://math.stackexchange.com/questions/871662/finding-right-quotient-of-languages
               , scope "DFA.rquotient" . expect $ and [ Config.accepts e3L1 [C, A, R, R, O, T]
                                                      , Config.accepts e3L2 [T]
@@ -28,17 +30,17 @@ suite = tests [ scope "DFA.literal"   . expect $ Config.language (DFA.literal Tr
                                                      , Prelude.take 2 (Config.language e3L1L2) == [[C, A, R, R], [C, A, R, R, O]]
                                                      ]
               ]
-    where e3L1 :: DFA Fin₈ Alpha
-          e3L1 = DFA { delta = δ
-                     , q0 = 0
-                     , fs = singleton 6
-                     } where δ (0, C) = 1
-                             δ (1, A) = 2
-                             δ (2, R) = 3
-                             δ (3, R) = 4
-                             δ (4, O) = 5
-                             δ (5, T) = 6
-                             δ _      = 7
-          e3L2 = DFA.union (right e3L1 4) (DFA.literal T)
+    where e3L1 ∷ DFA Fin₈ Alpha
+          e3L1   = DFA { delta = δ
+                        , q0 = 0
+                        , fs = singleton 6
+                        } where δ (0, C) = 1
+                                δ (1, A) = 2
+                                δ (2, R) = 3
+                                δ (3, R) = 4
+                                δ (4, O) = 5
+                                δ (5, T) = 6
+                                δ _      = 7
+          e3L2   = DFA.union (right e3L1 4) (DFA.literal T)
           -- e3L2 = DFA.union (right e3L1 4) (right e3L1 5)
           e3L1L2 = DFA.rquotient e3L1 e3L2
