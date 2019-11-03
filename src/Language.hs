c17fbc93f32b422060035cdb1cfc5a9edd68b74b
Ian Treyball <subttle@users.noreply.github.com>
2019-11-03 05:20:49 -0500

    clean up refactor comments in Language.hs

--- src/Language.hs
+++ src/Language.hs
@@ -25,8 +25,6 @@ instance (Finite s) ⇒ Σ (ℒ s) s
 -- provided for convenience/clarity.
 -- ℓ `accepts` w ≡ ℓ w
 accepts ∷ ℒ s → [s] → Bool
--- accepts (Predicate ℓ) w = ℓ w
--- accepts (Predicate ℓ) = ℓ
 accepts = getPredicate
 
 
@@ -40,17 +38,9 @@ empty = Predicate (const False)
 
 -- the language which accepts only the empty string
 epsilon ∷ ℒ s
-{-
-epsilon = Predicate p
-  where p [] = True
-        p _  = False
--}
 epsilon = Predicate null
 
 -- the language which accepts only a single literal
--- lit ∷ (Eq s) ⇒ s → ℒ s
--- lit σ [σ'] = σ == σ'
--- lit _ _    = False
 -- want version of `null` for NE? `isSingleton`?
 lit ∷ forall s . (Eq s) ⇒ s → ℒ s
 lit σ = Predicate p
@@ -75,12 +65,7 @@ concatenate ∷ ℒ s → ℒ s → ℒ s
 concatenate  (Predicate ℓ₁) (Predicate ℓ₂) = Predicate (\w → any (\(w₁ , w₂) → ℓ₁ w₁ ∧ ℓ₂ w₂) (zip (inits w) (tails w)))
 
 -- Kleene star
--- star ∷ ℒ s → ℒ s
--- star _ [] = True
--- star ℓ w  = any (all (ℓ . NE.toList)) (partitions w)
 star ∷ forall s . ℒ s → ℒ s
--- star _ [] = True
--- star (Predicate ℓ) = Predicate (\w → any (all (ℓ . NE.toList)) (partitions w))
 star (Predicate ℓ) = Predicate p
   where
     -- TODO express in terms of `epsilon + any ...`?
@@ -91,21 +76,14 @@ star (Predicate ℓ) = Predicate p
 
 -- inverse homomorphism
 invhom ∷ ([s] → [g]) → ℒ g → ℒ s
--- invhom h (Predicate ℓ) = Predicate (ℓ . h)
 invhom = contramap
 
 -- inverse homomorphic image of ℓ under h
--- invhomimage ∷ (s → [g]) → ℒ g → ℒ s
--- invhomimage h ℓ = ℓ . concatMap h
 invhomimage ∷ (s → [g]) → ℒ g → ℒ s
--- invhomimage h (Predicate ℓ) = Predicate (ℓ . concatMap h)
 invhomimage h = contramap (concatMap h)
 
 -- ε-free inverse homomorphic image of ℓ under h
--- invhomimageEpsFree ∷ (s → NE.NonEmpty g) → ℒ g → ℒ s
--- invhomimageEpsFree h ℓ = ℓ . concatMap (NE.toList . h)
 invhomimageEpsFree ∷ (s → NE.NonEmpty g) → ℒ g → ℒ s
--- invhomimageEpsFree h (Predicate ℓ) = Predicate (ℓ . concatMap (NE.toList . h))
 invhomimageEpsFree h = contramap (concatMap (NE.toList . h))
 
 invhomimagew ∷ (Eq g) ⇒ (s → [g]) → [g] → ℒ s
@@ -113,12 +91,10 @@ invhomimagew h w = Predicate ((w ==) . concatMap h)
 
 -- derivative with respect to some symbol in Σ
 derivative ∷ ℒ s → s → ℒ s
--- derivative ℓ a w = ℓ (a : w)
 derivative (Predicate ℓ) a = Predicate (\w → ℓ (a : w))
 
 -- derivative with respect to some word ∈ Σ★
 derivative' ∷ ℒ s → [s] → ℒ s
--- derivative' ℓ v w = ℓ (v ++ w)
 derivative' (Predicate ℓ) w₁ = Predicate (\w₂ → ℓ (w₁ ++ w₂))
 
 -- some useful instances are defined over this type
