357e65e5581615288573299f8e7d4f28890af0bc
Ian Treyball <536128+subttle@users.noreply.github.com>
2019-11-12 15:56:33 -0500

    improve consistency for EFA

--- src/EFA.hs
+++ src/EFA.hs
@@ -20,7 +20,6 @@ import qualified TransitionGraph as TG
 import qualified FA
 import qualified RegExp as RE
 import qualified ERE as Ex
--- import           Algebra.Graph.Relation as Relation
 import           Config
 
 -- Nondeterministic Finite Automaton with ε-transitions
@@ -48,6 +47,7 @@ instance Contravariant SomeEFA where
   contramap h (SomeEFA m) = SomeEFA (contramap h m)
 
 instance (Show q, Finite q, Show s, Finite s) ⇒ Show (EFA q s) where
+  show ∷ EFA q s → String
   show m@(EFA _ q₀ f) = List.intercalate "\n, "
                         [ "( Q  = "                      ++ (show . Set' . qs)                m
                         ,   "Σ  = "                      ++ (show . Set' . sigma)             m
@@ -129,44 +129,43 @@ table = Map.toAscList . deltaToMap
 -- ε-closure of a set of states
 -- Computes the set of states accessible from given state when no input can be consumed (0 or more ε-transitions)
 -- Always contains at least the original state
-eclosure ∷ (Ord q) ⇒ EFA q s → Set q → Set q
+eclosure ∷ ∀ q s . (Ord q) ⇒ EFA q s → Set q → Set q
 eclosure (EFA δ _ _) = fixedPoint closure
-        where closure = foldMap (\q → q `Set.insert` δ (q, Nothing))
+  where
+    closure ∷ Set q → Set q
+    closure = foldMap (\q → q `Set.insert` δ (q, Nothing))
 
 -- The EFA, empty, such that
 -- ℒ(empty) = ∅
 -- while adhering to Thompson's construction constraints
 empty ∷ EFA Bool s
-empty = EFA { delta = const (∅)
-            , q0    = False
-            , fs    = singleton True
-            }
+empty = EFA (const (∅)) False (singleton True)
 
 -- The EFA, epsilon, such that
 -- ℒ(epsilon) = {ε}
 -- while adhering to Thompson's construction constraints
 epsilon ∷ EFA Bool s
-epsilon = EFA { delta = δ
-              , q0    = False
-              , fs    = singleton True
-              } where δ (False, Nothing) = singleton True
-                      δ _                = (∅)
+epsilon = EFA δ False (singleton True)
+  where
+    δ ∷ (Bool, Maybe s) → Set Bool
+    δ (False, Nothing) = singleton True
+    δ _                = (∅)
 
 -- Given a symbol, construct an EFA which recognizes exactly that symbol and nothing else
-literal ∷ (Eq s) ⇒ s → EFA Bool s
-literal σ' = EFA { delta = δ
-                 , q0    = False
-                 , fs    = singleton True
-                 } where δ (False, Just σ) | σ == σ' = singleton True
-                         δ _                         = (∅)
+literal ∷ ∀ s . (Eq s) ⇒ s → EFA Bool s
+literal σ' = EFA δ False (singleton True)
+  where
+    δ ∷ (Bool, Maybe s) → Set Bool
+    δ (False, Just σ) | σ == σ' = singleton True
+    δ _                         = (∅)
 
 -- Treat the given set as a set of literals to define a language
-fromSet ∷ (Ord s) ⇒ Set s → EFA Bool s
-fromSet s = EFA { delta = δ
-                , q0    = False
-                , fs    = singleton True
-                } where δ (False, Just σ) | σ ∈ s = singleton True
-                        δ _                       = (∅)
+fromSet ∷ ∀ s . (Ord s) ⇒ Set s → EFA Bool s
+fromSet s = EFA δ False (singleton True)
+  where
+    δ ∷ (Bool, Maybe s) → Set Bool
+    δ (False, Just σ) | σ ∈ s = singleton True
+    δ _                       = (∅)
 
 -- Avoid using `foldl` because the base case may introduce extra states
 fromList ∷ (Eq s) ⇒ [s] → SomeEFA s
@@ -201,7 +200,7 @@ fromRE (RE.Star α) = star'        (fromRE α)
                      star'                     (SomeEFA m)  = SomeEFA (EFA.star m)
 
 -- Thompson's construction on regular expressions extended with intersection
-fromERE ∷ forall s . (Eq s) ⇒ Ex.ExRE s → SomeEFA s
+fromERE ∷ ∀ s . (Eq s) ⇒ Ex.ExRE s → SomeEFA s
 fromERE Ex.Zero     = SomeEFA EFA.empty
 fromERE Ex.One      = SomeEFA epsilon
 fromERE (Ex.Lit  σ) = SomeEFA (literal σ)
@@ -223,67 +222,64 @@ fromERE (Ex.Star α) = star'        (fromERE α)
 -- TODO in all the other functions (noted with "for Thompson's construction") but the preliminary
 -- TODO testing I've done so far seems to indicate that it works
 -- Pg 58. http://www.dcc.fc.up.pt/~nam/web/resources/rafaelamsc.pdf
-synchronous ∷ forall q p s . (Ord q, Ord p) ⇒ EFA q s → EFA p s → EFA (q, p) s
-synchronous (EFA δ₁ q₀ f₁) (EFA δ₂ p₀ f₂) = EFA { delta = δ
-                                                , q0    = (q₀, p₀)
-                                                , fs    = f₁ × f₂
-                                                } where δ ∷ ((q, p), Maybe s) → Set (q, p)
-                                                        δ ((q, p), Nothing) = (δ₁ (q, Nothing) × δ₂ (p, Nothing))
-                                                                            ∪  δ₁ (q, Nothing) × singleton p
-                                                                            ∪ singleton q      × δ₂ (p, Nothing)
-                                                        δ ((q, p), σ)       =  δ₁ (q, σ)       × δ₂ (p, σ)
+synchronous ∷ ∀ q p s . (Ord q, Ord p) ⇒ EFA q s → EFA p s → EFA (q, p) s
+synchronous (EFA δ₁ q₀ f₁) (EFA δ₂ p₀ f₂) = EFA δ (q₀, p₀) (f₁ × f₂)
+  where
+    δ ∷ ((q, p), Maybe s) → Set (q, p)
+    δ ((q, p), Nothing) = (δ₁ (q, Nothing) × δ₂ (p, Nothing))
+                        ∪  δ₁ (q, Nothing) × singleton p
+                        ∪ singleton q      × δ₂ (p, Nothing)
+    δ ((q, p), σ)       =  δ₁ (q, σ)       × δ₂ (p, σ)
 
 -- Union for Thompson's construction
-union ∷ (Ord q, Ord p) ⇒ EFA q s → EFA p s → EFA (Either Bool (Either q p)) s
-union (EFA δ₁ q₀ f₁) (EFA δ₂ p₀ f₂) =
-                        EFA { delta = δ
-                            , q0    = Left False
-                            , fs    = Set.singleton (Left True)
-                            } where δ (Left      False, Nothing)          = Set.fromList [Right (Left q₀), Right (Right p₀)]
-                                    δ (Left          _,       _)          = (∅)
-                                    -- connect the "old" final states of m₁ to the new final state
-                                    δ (Right (Left  q), Nothing) | q ∈ f₁ = Left True `Set.insert` Set.map (Right . Left)  (δ₁ (q, Nothing))
-                                    δ (Right (Left  q),       σ)          =                        Set.map (Right . Left)  (δ₁ (q,       σ))
-                                    δ (Right (Right p), Nothing) | p ∈ f₂ = Left True `Set.insert` Set.map (Right . Right) (δ₂ (p, Nothing))
-                                    δ (Right (Right p),       σ)          =                        Set.map (Right . Right) (δ₂ (p,       σ))
+union ∷ ∀ q p s . (Ord q, Ord p) ⇒ EFA q s → EFA p s → EFA (Either Bool (Either q p)) s
+union (EFA δ₁ q₀ f₁) (EFA δ₂ p₀ f₂) = EFA δ (Left False) (Set.singleton (Left True))
+  where
+    δ ∷ (Either Bool (Either q p), Maybe s) → Set (Either Bool (Either q p))
+    δ (Left      False, Nothing)          = Set.fromList [Right (Left q₀), Right (Right p₀)]
+    δ (Left          _,       _)          = (∅)
+    -- connect the "old" final states of m₁ to the new final state
+    δ (Right (Left  q), Nothing) | q ∈ f₁ = Left True `Set.insert` Set.map (Right . Left)  (δ₁ (q, Nothing))
+    δ (Right (Left  q),       σ)          =                        Set.map (Right . Left)  (δ₁ (q,       σ))
+    δ (Right (Right p), Nothing) | p ∈ f₂ = Left True `Set.insert` Set.map (Right . Right) (δ₂ (p, Nothing))
+    δ (Right (Right p),       σ)          =                        Set.map (Right . Right) (δ₂ (p,       σ))
 
 -- Concatenation for Thompson's construction
-concatenate ∷ (Ord q, Ord p) ⇒ EFA q s → EFA p s → EFA (Either q p) s
-concatenate (EFA δ₁ q₀ f₁) (EFA δ₂ p₀ f₂) =
-                                      EFA { delta = δ
-                                          , q0    = Left q₀
-                                          , fs    = map Right f₂
-                                          -- if this state, q, is a final state, merge q's transitions with p₀'s transitions
-                                          } where δ (Left  q, σ) | q ∈ f₁ =           δ₁ (q, σ) ⊎ δ₂ (p₀, σ)
-                                                  δ (Left  q, σ)          = map Left (δ₁ (q, σ))
-                                                  δ (Right p, σ)          =            map Right (δ₂ (p,  σ))
+concatenate ∷ ∀ q p s . (Ord q, Ord p) ⇒ EFA q s → EFA p s → EFA (Either q p) s
+concatenate (EFA δ₁ q₀ f₁) (EFA δ₂ p₀ f₂) = EFA δ (Left q₀) (map Right f₂)
+  where
+    δ ∷ (Either q p, Maybe s) → Set (Either q p)
+    -- if this state, q, is a final state, merge q's transitions with p₀'s transitions
+    δ (Left  q, σ) | q ∈ f₁ =           δ₁ (q, σ) ⊎ δ₂ (p₀, σ)
+    δ (Left  q, σ)          = map Left (δ₁ (q, σ))
+    δ (Right p, σ)          =            map Right (δ₂ (p,  σ))
 
 -- Kleene Star for Thompson's construction
-star ∷ (Ord q) ⇒ EFA q s → EFA (Either Bool q) s
-star (EFA δ q₀ f) = EFA { delta = δ₁
-                        , q0    = Left False
-                        , fs    = singleton (Left True)
-                        } where δ₁ (Left False, Nothing)         =                              Set.fromList [Right q₀, Left True]
-                                δ₁ (Left     _,       _)         = (∅)
-                                δ₁ (Right    q, Nothing) | q ∈ f = map Right (δ (q, Nothing)) ∪ Set.fromList [Right q₀, Left True]
-                                δ₁ (Right    q,       σ)         = map Right (δ (q,       σ))
+star ∷ ∀ q s . (Ord q) ⇒ EFA q s → EFA (Either Bool q) s
+star (EFA δ q₀ f) = EFA δ₁ (Left False) (singleton (Left True))
+  where
+    δ₁ ∷ (Either Bool q, Maybe s) → Set (Either Bool q)
+    δ₁ (Left False, Nothing)         =                              Set.fromList [Right q₀, Left True]
+    δ₁ (Left     _,       _)         = (∅)
+    δ₁ (Right    q, Nothing) | q ∈ f = map Right (δ (q, Nothing)) ∪ Set.fromList [Right q₀, Left True]
+    δ₁ (Right    q,       σ)         = map Right (δ (q,       σ))
 
 -- FIXME test me, I wrote this when tired
 -- The asynchronous product of two EFA
 -- Essentially this runs two EFAs with different alphabets "in parallel" independently
-asynchronous ∷ (Ord q, Ord p) ⇒ EFA q s → EFA p g → EFA (q, p) (Either s g)
-asynchronous (EFA δ₁ q₀ f₁) (EFA δ₂ p₀ f₂) = EFA { delta = δ
-                                                 , q0    = (q₀, p₀)
-                                                 , fs    = f₁ × f₂
-                                                 } where δ ((q, p), Nothing)        = δ₁ (q, Nothing) × δ₂ (p, Nothing)
-                                                         δ ((q, p), Just (Left  σ)) = δ₁ (q, Just  σ) × singleton p
-                                                         δ ((q, p), Just (Right γ)) = singleton q     × δ₂ (p, Just  γ)
+asynchronous ∷ ∀ q p s g . (Ord q, Ord p) ⇒ EFA q s → EFA p g → EFA (q, p) (Either s g)
+asynchronous (EFA δ₁ q₀ f₁) (EFA δ₂ p₀ f₂) = EFA δ (q₀, p₀) (f₁ × f₂)
+  where
+    δ ∷ ((q, p), Maybe (Either s g)) → Set (q, p)
+    δ ((q, p), Nothing)        = δ₁ (q, Nothing) × δ₂ (p, Nothing)
+    δ ((q, p), Just (Left  σ)) = δ₁ (q, Just  σ) × singleton p
+    δ ((q, p), Just (Right γ)) = singleton q     × δ₂ (p, Just  γ)
 
 -- TODO version which determinizes instead of adding an extra state
-fromFA ∷ (Ord q) ⇒ FA.FA q s → EFA (Either () q) s
-fromFA (FA.FA δ i f) = EFA { delta = Set.map Right . δ₁
-                           , q0    = Left ()
-                           , fs    = Set.map Right f
-                           } where δ₁ (Left (), Nothing) = i
-                                   δ₁ (Right q, Just  σ) = δ (q, σ)
-                                   δ₁ _                  = (∅)
+fromFA ∷ ∀ q s . (Ord q) ⇒ FA.FA q s → EFA (Either () q) s
+fromFA (FA.FA δ i f) = EFA (Set.map Right . δ₁) (Left ()) (Set.map Right f)
+  where
+    δ₁ ∷ (Either () q, Maybe s) → Set q
+    δ₁ (Left (), Nothing) = i
+    δ₁ (Right q, Just  σ) = δ (q, σ)
+    δ₁ _                  = (∅)
