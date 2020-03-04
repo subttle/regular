ef3618f87f8f6bfa6575c6816c43bb7369e3b50e
Ian <subttle@users.noreply.github.com>
2020-03-04 13:52:51 -0500

    point-free in GNFA.lmap

--- src/GNFA.hs
+++ src/GNFA.hs
@@ -12,14 +12,15 @@ import           Finite
 import           Language (ℒ)
 import           RegExp (RegExp)
 import           RegExp as RE
+import           Control.Selective (Selective, select, (<*?))
 import qualified Data.List as List
 import           Data.Set as Set
+import           Data.Bifunctor (bimap)
 import           Data.Bool.Unicode ((∨))
 import qualified Data.Map as Map (fromList)
-import           Control.Selective (Selective, select, (<*?))
-import           Data.Void (Void, absurd)
 import           Data.Pointed (Pointed, point)
 import           Data.Profunctor (Profunctor, lmap, rmap)
+import           Data.Void (Void, absurd)
 
 -- Generalization of Nondeterministic Finite Automaton with ε-transitions
 
@@ -35,6 +36,7 @@ instance (Finite q) ⇒ Q (GNFA q s) (Either (Either Init Final) q)
 instance (Finite s) ⇒ Σ (GNFA q s) s
 
 instance Pointed (GNFA Void) where
+  point ∷ s → GNFA Void s
   point = GNFA.literal
 
 instance Pointed (GNFA q) where
@@ -60,7 +62,7 @@ instance Profunctor GNFA where
   rmap ∷ (s → g) → GNFA q s → GNFA q g
   rmap = fmap
   lmap ∷ (p → q) → GNFA q s → GNFA p s
-  lmap f (GNFA δ) = GNFA (\(p₁, p₂) → δ (fmap f p₁, fmap f p₂))
+  lmap h (GNFA δ) = GNFA (δ . bimap (fmap h) (fmap h))
 
 instance (Show q, Finite q, Show s, Finite s)
        ⇒ Show (GNFA q s) where
@@ -84,26 +86,27 @@ toLanguage = RE.toLanguage . toRE
 
 table ∷ ∀ q s . (Finite q) ⇒ GNFA q s → [((Either Init q, Either Final q), RegExp s)]
 table (GNFA δ) = zip domain image
-    where
-      domain ∷ [(Either Init q, Either Final q)]
-      domain = asList
-      image ∷ [RegExp s]
-      image  = fmap δ domain
+  where
+    domain ∷ [(Either Init q, Either Final q)]
+    domain = asList
+    image ∷ [RegExp s]
+    image  = fmap δ domain
 
 -- Rip out all of `q` leaving only a two state GNFA (only the two qᵢ and qᶠ states)
 reduce ∷ (Finite q, Ord s) ⇒ GNFA q s → GNFA Void s
-reduce m = lmap absurd (Set.foldl rip m asSet)
+reduce = lmap absurd . flip (Set.foldl rip) asSet
 
 -- δ₁(q, p) = δ(q, r) ⊗ δ(r, r)⋆ ⊗ δ(r, p) ⊕ δ(q, p) where q, p, r ∈ Q, and r is the state to "rip"
 rip ∷ ∀ q s . (Eq q, Ord s) ⇒ GNFA q s → q → GNFA q s
-rip (GNFA δ) qᵣ' = GNFA δ₁
-  where qᵣ ∷ Either a q
-        qᵣ = Right qᵣ'
-        δ₁ ∷ (Either Init q, Either Final q) → RegExp s
-        δ₁ (q₁, q₂) | (q₁ == qᵣ) ∨ (q₂ == qᵣ) = zero -- We are ripping qᵣ out, so if qᵣ is an arg to δ₁, return Zero
-        δ₁ (q₁, q₂)                           = δ (q₁, qᵣ) * (star (δ (qᵣ, qᵣ)) * δ (qᵣ, q₂)) + δ (q₁, q₂)
-        -- or
-        -- δ₁ (q₁, q₂)                           = δ (q₁, q₂) + (δ (q₁, qᵣ) * (star (δ (qᵣ, qᵣ)) * δ (qᵣ, q₂)))
+rip (GNFA δ) q = GNFA δ₁
+  where
+    qᵣ ∷ Either a q
+    qᵣ = Right q
+    δ₁ ∷ (Either Init q, Either Final q) → RegExp s
+    δ₁ (q₁, q₂) | (q₁ == qᵣ) ∨ (q₂ == qᵣ) = zero -- We are ripping qᵣ out, so if qᵣ is an arg to δ₁, return Zero
+    δ₁ (q₁, q₂)                           = δ (q₁, qᵣ) * (star (δ (qᵣ, qᵣ)) * δ (qᵣ, q₂)) + δ (q₁, q₂)
+    -- or
+    -- δ₁ (q₁, q₂)                           = δ (q₁, q₂) + (δ (q₁, qᵣ) * (star (δ (qᵣ, qᵣ)) * δ (qᵣ, q₂)))
 
 extract ∷ GNFA Void s → RegExp s
 extract (GNFA δ) = δ (Left (Init ()), Left (Final ()))
