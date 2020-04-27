3a6274c921c1c47d8a861e13ea588acccc6700e9
Ian Treyball <536128+subttle@users.noreply.github.com>
2020-04-27 19:01:15 -0400

    small consistency update

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,20 @@
-import DFA
-import Examples
-import Common
-import Config
+import DFA (intersection)
+import Common (toDigits)
+import Config (accepts)
+import Examples (by3, by5)
+import Finite (Fin₁₀)
 
 -- FizzBuzz using DFA
 main ∷ IO ()
 main = mapM_ (putStrLn . fizzbuzz . toDigits) [1 .. 100]
   where
+    fizz ∷ [Fin₁₀] → Bool
     fizz = accepts  by3
+    buzz ∷ [Fin₁₀] → Bool
     buzz = accepts                         by5
+    fbzz ∷ [Fin₁₀] → Bool
     fbzz = accepts (by3 `DFA.intersection` by5)
+    fizzbuzz ∷ [Fin₁₀] → String
     fizzbuzz n | fbzz n    = "FizzBuzz"
                | fizz n    = "Fizz"
                | buzz n    = "Buzz"
