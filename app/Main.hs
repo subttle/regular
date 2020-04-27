ddf5ab03463eecaf05131f0b1b6f4db2e7cbee98
Ian Treyball <536128+subttle@users.noreply.github.com>
2020-04-27 18:56:03 -0400

    small consistency update

--- app/Main.hs
+++ app/Main.hs
@@ -6,11 +6,11 @@ import Config
 -- FizzBuzz using DFA
 main ∷ IO ()
 main = mapM_ (putStrLn . fizzbuzz . toDigits) [1 .. 100]
-       where fizz = accepts  by3
-             buzz = accepts                         by5
-             fbzz = accepts (by3 `DFA.intersection` by5)
-             fizzbuzz n
-               | fbzz n    = "FizzBuzz"
+  where
+    fizz = accepts  by3
+    buzz = accepts                         by5
+    fbzz = accepts (by3 `DFA.intersection` by5)
+    fizzbuzz n | fbzz n    = "FizzBuzz"
                | fizz n    = "Fizz"
                | buzz n    = "Buzz"
                | otherwise = n >>= show
