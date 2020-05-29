19b54693396478b4d1346763be12e577962a221d
Ian Treyball <536128+subttle@users.noreply.github.com>
2020-05-29 00:12:20 -0400

    progress with FreeGroup experimenting

--- src/IntBase.hs
+++ src/IntBase.hs
@@ -5,6 +5,9 @@
 
 module IntBase where
 
+import           Control.Applicative (empty, (<|>), Alternative)
+import           Control.Monad (ap)
+import           Data.Functor ((<&>))
 import           Data.Functor.Foldable (Fix (..), Recursive, Corecursive, Base, project, embed)
 import           Data.Function (on)
 import           Data.Group (Group, invert)
@@ -12,7 +15,7 @@ import           Data.List (unfoldr)
 import           Data.List.NonEmpty (NonEmpty, NonEmpty ((:|)), (<|))
 import qualified Data.List.NonEmpty as NE
 import           GHC.Real (reduce)
-import           Common (ordering)
+import           Common ((‥), ordering)
 
 -- N.B. this entire file is currently experimental/untested/WIP!
 
@@ -158,3 +161,93 @@ telescope = unfoldr c
     c (Prev i) = Just (Left  i, i)
     c Zero     = Nothing
     c (Next i) = Just (Right i, i)
+
+{-
+-- FIXME untested
+-- TODO trying to figure out best way to represent this for fun with transition monoid :)
+--
+-- TODO https://planetmath.org/freegroup
+-}
+data FreeGroup a where
+  Zer ∷ FreeGroup a
+  Neg ∷ a → (FreeGroup a → FreeGroup a)
+  Pos ∷ a → (FreeGroup a → FreeGroup a)
+
+-- case analysis
+freegroup ∷ b → (a → (b → b)) → (a → (b → b)) → (FreeGroup a → b)
+-- freegroup ∷ b → (a → b → b) → (a → b → b) → FreeGroup a → b
+freegroup z _ _ Zer        = z
+freegroup z n p (Neg a ga) = n a (freegroup z n p ga)
+freegroup z n p (Pos a ga) = p a (freegroup z n p ga)
+
+instance (Show a) ⇒ Show (FreeGroup a) where
+  -- FIXME best way to show entire thing (symbols, etc)
+  show ∷ FreeGroup a → String
+  show Zer        = "0"
+  show (Neg a ga) = "(-" <> show a <> "TODO" <> ")" -- FIXME
+  show (Pos a ga) = "(+" <> show a <> "TODO" <> ")" -- FIXME
+
+instance Semigroup (FreeGroup a) where
+  (<>) ∷ FreeGroup a → FreeGroup a → FreeGroup a
+  (<>) Zer        r  =             r
+  (<>)        l  Zer =        l
+  (<>) (Pos a l) r   = Pos a (l <> r)
+  (<>) (Neg a l) r   = Neg a (l <> r)
+instance Monoid (FreeGroup a) where
+  mempty ∷ FreeGroup a
+  mempty = Zer
+instance Group (FreeGroup a) where
+  -- FIXME: untested
+  invert ∷ FreeGroup a → FreeGroup a
+  invert = freegroup Zer Pos Neg
+
+instance Functor FreeGroup where
+  fmap ∷ (a → b) → FreeGroup a → FreeGroup b
+  fmap _ Zer        = Zer
+  fmap f (Neg a ga) = Neg (f a) (fmap f ga)
+  fmap f (Pos a ga) = Pos (f a) (fmap f ga)
+
+instance Foldable FreeGroup where
+  -- TODO
+  foldMap ∷ (Monoid m) ⇒ (a → m) → FreeGroup a → m
+  foldMap _ Zer        = mempty
+  foldMap f (Neg a ga) = f a <> foldMap f ga
+  foldMap f (Pos a ga) = f a <> foldMap f ga
+
+instance Traversable FreeGroup where
+  -- TODO
+  traverse ∷ (Applicative f) ⇒ (a → f b) → FreeGroup a → f (FreeGroup b)
+  traverse _ Zer        = pure Zer
+  traverse f (Neg a ga) = Neg <$> f a <*> traverse f ga
+  traverse f (Pos a ga) = Pos <$> f a <*> traverse f ga
+
+instance Applicative FreeGroup where
+  -- FIMXE may also want to newtype
+  {-
+  -- https://en.wikibooks.org/wiki/Haskell/Applicative_functors#Applicative_functor_laws
+  pure id <*> v = v                            -- Identity
+  pure f <*> pure x = pure (f x)               -- Homomorphism
+  u <*> pure y = pure ($ y) <*> u              -- Interchange
+  pure (.) <*> u <*> v <*> w = u <*> (v <*> w) -- Composition
+  -}
+  pure ∷ a → FreeGroup a
+  -- pure a = Pos a Zer
+  pure = flip Pos Zer -- TODO
+
+  (<*>) ∷ FreeGroup (a → b) → FreeGroup a → FreeGroup b
+  (<*>) = ap
+
+instance Monad FreeGroup where
+  (>>=) ∷ FreeGroup a → (a → FreeGroup b) → FreeGroup b
+  (>>=) = join ‥ (<&>)
+    where
+      join ∷ FreeGroup (FreeGroup a) → FreeGroup a
+      join Zer          = Zer
+      join (Neg ga gga) = invert ga <> join gga
+      join (Pos ga gga) =        ga <> join gga
+
+instance Alternative FreeGroup where
+  empty ∷ FreeGroup a
+  empty = Zer
+  (<|>) ∷ FreeGroup a → FreeGroup a → FreeGroup a
+  (<|>) = undefined -- TODO
