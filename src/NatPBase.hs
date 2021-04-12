e94d7f6a4d072d2d5a3a7423456f43272dae633a
Ian Treyball <536128+subttle@users.noreply.github.com>
2021-04-12 19:46:15 -0400

    add initial non-zero natural number support

---
+++ src/NatPBase.hs
@@ -0,0 +1,72 @@
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE DeriveTraversable  #-}
+
+module NatPBase where
+
+import           Data.Function (on)
+import           Data.Functor.Contravariant (Predicate (..))
+import           Numeric.Natural (Natural)
+import           Prelude hiding (even, odd)
+
+
+-- N.B. this entire file is currently experimental/untested/WIP!
+
+-- TODO ideally can just reuse most of src/NatBase.hs but going to want to check edge cases
+
+
+-- Ideally would name this type `ℕ⁺`, to represent ℕ \ {0}, but the GHC doesn't like that
+data ℕ¹ where
+  One ∷      ℕ¹
+  Suc ∷ ℕ¹ → ℕ¹
+
+type Natural1 = ℕ¹
+
+-- case analysis
+nat1 ∷ a → (a → a) → ℕ¹ → a
+nat1 o _ One     = o
+nat1 o s (Suc n) = s (nat1 o s n)
+
+data Nat1F a where
+  OneF ∷     Nat1F a
+  SucF ∷ a → Nat1F a
+deriving instance Functor     Nat1F
+deriving instance Foldable    Nat1F
+deriving instance Traversable Nat1F
+
+toNatural ∷ ℕ¹ → Natural
+toNatural = nat1 1 succ
+
+-- FIXME TODO
+instance Num ℕ¹ where
+  abs ∷ ℕ¹ → ℕ¹
+  abs = id
+
+instance Show ℕ¹ where
+  show ∷ ℕ¹ → String
+  show = show . toNatural
+
+-- N.B. this is not a bijection!
+instance Enum ℕ¹ where
+  toEnum ∷ Int → ℕ¹
+  toEnum i | i == 1 = One
+           | i >  1 = Suc (toEnum (i - 1))
+           | i <  1 = error "toEnum"
+  fromEnum ∷ ℕ¹ → Int
+  fromEnum = nat1 1 succ
+  succ ∷ ℕ¹ → ℕ¹
+  succ = Suc
+  pred ∷ ℕ¹ → ℕ¹
+  pred  One    = error "pred"
+  pred (Suc n) = n
+instance Eq ℕ¹ where
+  (==) ∷ ℕ¹ → ℕ¹ → Bool
+  (==) = (==) `on` toNatural
+instance Ord ℕ¹ where
+  compare ∷ ℕ¹ → ℕ¹ → Ordering
+  compare = compare `on` toNatural
+
+even ∷ Predicate ℕ¹
+even = Predicate (nat1 False not)
+
+odd ∷ Predicate ℕ¹
+odd = Predicate (nat1 True not)
